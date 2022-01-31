3459fb23fcdc7a6eea0dd63a55fd060f52b618b2
quent <quentin.mary@reseau.eseo.fr>
2022-01-31 11:42:13 +0100

    Bases du Projet sans fonction

--- app/src/main/java/com/example/memoryv2/MainActivity.java
+++ app/src/main/java/com/example/memoryv2/MainActivity.java
@@ -2,9 +2,12 @@ package com.example.memoryv2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -14,6 +17,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private ImageView image4;
     private ImageView image5;
     private ImageView image6;
+
+    private ImageView carte_prec = null;
 
 
     @Override
@@ -31,6 +36,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     @Override
     protected void onResume() {
         super.onResume();
+        image1.setOnClickListener(this);
+        image2.setOnClickListener(this);
+        image3.setOnClickListener(this);
+        image4.setOnClickListener(this);
+        image5.setOnClickListener(this);
+        image6.setOnClickListener(this);
+
 
     }
     /*public boolean est_retourne(){
@@ -39,6 +51,54 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
+        int id = v.getId();
+        if(id == R.id.image1){
+            ImageView imageVue = v.findViewById(id);
+            imageVue.setImageResource(R.drawable.fraise);
+        }
+        else if (id == R.id.image2){
+            ImageView imageVue = v.findViewById(id);
+            imageVue.setImageResource(R.drawable.peche);
+        }
+        else if (id == R.id.image3){
+            ImageView imageVue = v.findViewById(id);
+            imageVue.setImageResource(R.drawable.pasteque);
+        }
+        else if (id == R.id.image4){
+            ImageView imageVue = v.findViewById(id);
+            imageVue.setImageResource(R.drawable.peche);
+        }
+        else if (id == R.id.image5){
+            ImageView imageVue = v.findViewById(id);
+            imageVue.setImageResource(R.drawable.fraise);
+        }
+        else if (id == R.id.image6){
+            ImageView imageVue = v.findViewById(id);
+            imageVue.setImageResource(R.drawable.pasteque);
+        }
+        if(carte_prec == null) {
+            carte_prec = v.findViewById(id);
+        }
+        else{
+            ImageView img1 = v.findViewById(id);
+            if(img1 != carte_prec){
+                Bitmap bimg1 = ((BitmapDrawable) img1.getDrawable()).getBitmap();
+                Bitmap bimg2 = ((BitmapDrawable) carte_prec.getDrawable()).getBitmap();
+                if(bimg1 == bimg2){
+                    Toast.makeText(MainActivity.this, "C'est gagné", Toast.LENGTH_LONG).show();
+                    carte_prec = null;
+                }
+                else{
+                    img1.setImageResource(R.drawable.backcard);
+                    carte_prec.setImageResource(R.drawable.backcard);
+                    carte_prec = null;
+                }
+            }else{
+                img1.setImageResource(R.drawable.backcard);
+                carte_prec.setImageResource(R.drawable.backcard);
+                carte_prec = null;
+            }
 
+        }
     }
 }
