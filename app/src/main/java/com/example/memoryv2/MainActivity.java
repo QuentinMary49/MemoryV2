8d0f9dbfc3ab07e00ca49d74343fd63ca9f2f5d6
quent <quentin.mary@reseau.eseo.fr>
2022-01-31 10:41:29 +0100

    Premier commit du nouveau projet

--- app/src/main/java/com/example/memoryv2/MainActivity.java
+++ app/src/main/java/com/example/memoryv2/MainActivity.java
@@ -3,12 +3,42 @@ package com.example.memoryv2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+
+    private ImageView image1;
+    private ImageView image2;
+    private ImageView image3;
+    private ImageView image4;
+    private ImageView image5;
+    private ImageView image6;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        image1 = findViewById(R.id.image1);
+        image2 = findViewById(R.id.image2);
+        image3 = findViewById(R.id.image3);
+        image4 = findViewById(R.id.image4);
+        image5 = findViewById(R.id.image5);
+        image6 = findViewById(R.id.image6);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+    }
+    /*public boolean est_retourne(){
+
+    }*/
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
